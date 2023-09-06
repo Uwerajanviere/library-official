d647a0cbda72d04d01c6299ba383e8e32459ced1
uwera janviere <janviereuwera43@gmail.com>
2023-09-05 20:46:28 -0700

    added three files

---
+++ signup-process.php
@@ -0,0 +1,10 @@
+<?php
+$firstname = $_POST['fname'];
+$lastname = $_POST['lname'];
+$email = $_POST['email'];
+$registration_number = $_POST['reg'];
+$school = $_POST['school'];
+$level = $_POST['level'];
+$program = $_POST['program']
+
+?>
